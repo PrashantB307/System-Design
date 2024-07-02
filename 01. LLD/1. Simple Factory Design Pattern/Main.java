458eb08e2bbdf301bea072e1a1b96ede4b0ff62a
PrashantB-307 <pb28.rkt@gmail.com>
2024-07-02 22:13:34 +0530

    Simple Factory Design Pattern

---
+++ 01. LLD/1. Simple Factory Design Pattern/Main.java
@@ -0,0 +1,18 @@
+import logger.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ILogger debugLogger = LoggerFactory.createLogger(LogLevel.DEBUG);
+        ILogger infoLogger = LoggerFactory.createLogger(LogLevel.INFO);
+        ILogger errorLogger = LoggerFactory.createLogger(LogLevel.ERROR);
+
+        debugLogger.log("This is a debug log msg");
+        infoLogger.log("This is an info log msg");
+        errorLogger.log("This is an error log msg");
+    }
+}
+
+// =======>   OUTPUT   <=======
+// DEBUG: This is a debug log msg
+// INFO: This is an info log msg
+// ERROR: This is an error log msg
